fea09a522def271fcad8d2a130489da7afc58de8
Maxim Shmelov <mokinous@gmail.com>
2025-12-23 19:15:11 +0000

    todos.ts actions

---
+++ src/db/actions/todos.ts
@@ -0,0 +1,16 @@
+import {Collection, CollectionType} from "@prisma/client";
+import {createCollection, getCollection, getCollections} from "@/src/db/actions/collections";
+
+const collectionType = CollectionType.TODO;
+
+export async function createTodo(title: string, familyId?: string): Promise<void> {
+  return createCollection(title, collectionType, familyId);
+}
+
+export async function getTodo(id: string): Promise<Collection> {
+  return getCollection(id, collectionType);
+}
+
+export async function getTodoList(): Promise<Collection> {
+  return getCollections(collectionType);
+}

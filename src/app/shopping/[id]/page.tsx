eb1e24fa75a4fa84b05cba3584e8541fa8c2731c
Maxim Shmelov <mokinous@gmail.com>
2025-12-23 18:52:08 +0000

    fixed redundant ownerId param in shopping/[id] page

--- src/app/shopping/[id]/page.tsx
+++ src/app/shopping/[id]/page.tsx
@@ -8,7 +8,7 @@ export default async function ShoppingList({params}: {searchParams: Promise<{id:
   if (!session) return "Loading";
   if (!paramsObj.id) return <div>No ID provided</div>;
 
-  const shopping: Omit<Collection, "type"> = await getShopping(session.user.id, paramsObj.id);
+  const shopping: Omit<Collection, "type"> = await getShopping(paramsObj.id);
 
   if (!shopping) return <div>404 Not Found</div>;
 

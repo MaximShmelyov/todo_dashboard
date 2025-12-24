9b6b44518251331772952a73b449d7ae01e0a28a
Maxim Shmelov <mokinous@gmail.com>
2025-12-24 19:55:29 +0000

    CollectionPage.tsx

---
+++ src/app/CollectionPage.tsx
@@ -0,0 +1,31 @@
+'use server'
+
+import {Collection, CollectionType} from "@prisma/client";
+import {getSession} from "@/src/lib/auth"
+import AddItemForm from "@/src/components/common/AddItemForm";
+import CollectionClient from "@/src/app/CollectionClient";
+import {getCollection} from "@/src/db/actions/collections";
+
+export default async function CollectionPage({collectionType, params, searchParams}: {
+  collectionType: CollectionType,
+  params: Promise<{ id: string }>,
+  searchParams: Promise<{ create?: string }>
+}) {
+  const session = await getSession();
+  const paramsObj = await params;
+  const searchParamsObj = await searchParams;
+  if (!session) return "Loading";
+  if (!paramsObj.id) return <div>No ID provided</div>;
+
+  const collection: Collection = await getCollection(paramsObj.id, collectionType);
+  const showForm = searchParamsObj.create === "1";
+
+  if (!collection) return <div>404 Not Found</div>;
+
+  return (
+    <>
+      <CollectionClient collection={collection}/>
+      {showForm && <AddItemForm collectionType={collectionType} collectionId={collection.id}/>}
+    </>
+  );
+}
--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -3,13 +3,13 @@
 import {CollectionType} from "@prisma/client";
 import CollectionPage from "@/src/app/CollectionPage";
 
-const collectionType = CollectionType.SHOPPING;
+const collectionType = CollectionType.NOTE;
 
-export default async function ShoppingList({params, searchParams}: {
+export default async function Note({params, searchParams}: {
   params: Promise<{ id: string }>,
   searchParams: Promise<{ create?: string }>
 }) {
   return (
-    <CollectionPage collectionType={collectionType} params={params} searchParams={searchParams}/>
+      <CollectionPage collectionType={collectionType} params={params} searchParams={searchParams}/>
   );
 }

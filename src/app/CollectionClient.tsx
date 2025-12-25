c33fb6d831d7e6fd5695b8c4e99220a4781a5449
Maxim Shmelov <mokinous@gmail.com>
2025-12-25 17:08:37 +0000

    CollectionClient.tsx layout update

--- src/app/CollectionClient.tsx
+++ src/app/CollectionClient.tsx
@@ -11,22 +11,21 @@ export default function CollectionClient({collection}: {
   return (
     <div className="">
       <BackNavigation href={getCollectionRoute(collection.type)}/>
-      <div className="text-lg font-medium">{collection.title}</div>
+      <div className="text-lg font-semibold">{collection.title}</div>
       <div>{collection.description}</div>
-      <div>{collection.family?.title ?? 'private'}</div>
+      <div>{collection.family?.title ?? '(private)'}</div>
       <div className="mt-2 py-2 shadow-sm">
-        <div className="">Items:</div>
         <ul className="flex flex-col gap-2 p-2">
           {collection.items.map(item => (
             <li
-              className="hover:bg-gray-100"
+              className="hover:bg-gray-100 p-2"
               key={item.id}
             >
-              <div className="font-medium">{item.title}</div>
-              <div>{item.body}</div>
-              <div>{item.dueDate && <div>Due date:{item.dueDate}</div>}</div>
-              <div>{item.updatedAt && ("Modified at: " + item.updatedAt?.toISOString())}</div>
-              <div>{"by " + item.createdBy.name}</div>
+              <div className="font-semibold">• {item.title}</div>
+              <div className="ml-4">{item.body}</div>
+              <div className="ml-4">{item.dueDate && <div>Due date:{item.dueDate}</div>}</div>
+              <div className="ml-4">{item.updatedAt && ("Modified at: " + item.updatedAt?.toISOString())}</div>
+              <div className="text-right w-full">{"by " + item.createdBy.name}</div>
             </li>
           ))}
         </ul>

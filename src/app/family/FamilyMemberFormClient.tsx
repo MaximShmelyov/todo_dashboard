04e72ea9836e18b290a09b63480305a66c94817c
Maxim Shmelov <mokinous@gmail.com>
2026-01-29 12:19:54 +0000

    Replace custom modal with Headless UI Dialog in CreateCollectionForm

--- src/app/family/FamilyMemberFormClient.tsx
+++ src/app/family/FamilyMemberFormClient.tsx
@@ -9,6 +9,8 @@ import {
   updateMembership,
 } from "@/src/db/actions/membership";
 import Button from "@/src/components/ui/Button";
+import ModalDialog from "@/src/components/common/ModalDialog";
+import ModalDialogTitle from "@/src/components/common/ModalDialogTitle";
 
 type FamilyMemberFormProps = {
   membership: MembershipExtended;
@@ -21,41 +23,36 @@ export default function FamilyMemberFormClient(props: FamilyMemberFormProps) {
   const { membership, onSuccessPath, inviteRoleTypes } = props;
 
   return (
-    <div
-      className="fixed inset-0 bg-black/40 flex justify-center items-center"
-      onClick={() => router.back()}
-    >
-      <div className="rounded-xl bg-white shadow-lg p-6" onClick={(e) => e.stopPropagation()}>
-        <Form
-          className="flex flex-col gap-4"
-          action={async (formData) => {
-            await updateMembership({
-              id: membership.id,
-              roleType: formData.get("roleType")!.toString() as RoleTypeLimited,
-            });
-            router.replace(onSuccessPath);
-          }}
-        >
-          <h3 className="text-lg text-center">
-            {membership.user.name} in {membership.family.name}
-          </h3>
-          <label>
-            Role:
-            <select
-              className="ml-1 hover:bg-stone-100"
-              name="roleType"
-              defaultValue={membership.roleType}
-            >
-              {inviteRoleTypes.map((roleType) => (
-                <option key={roleType} value={roleType}>
-                  {roleType}
-                </option>
-              ))}
-            </select>
-          </label>
-          <Button type="submit">Submit</Button>
-        </Form>
-      </div>
-    </div>
+    <ModalDialog onCloseAction={() => router.back()}>
+      <Form
+        className="flex flex-col gap-4"
+        action={async (formData) => {
+          await updateMembership({
+            id: membership.id,
+            roleType: formData.get("roleType")!.toString() as RoleTypeLimited,
+          });
+          router.replace(onSuccessPath);
+        }}
+      >
+        <ModalDialogTitle>
+          {membership.user.name} in {membership.family.name}
+        </ModalDialogTitle>
+        <label>
+          Role:
+          <select
+            className="ml-1 hover:bg-stone-100"
+            name="roleType"
+            defaultValue={membership.roleType}
+          >
+            {inviteRoleTypes.map((roleType) => (
+              <option key={roleType} value={roleType}>
+                {roleType}
+              </option>
+            ))}
+          </select>
+        </label>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </ModalDialog>
   );
 }

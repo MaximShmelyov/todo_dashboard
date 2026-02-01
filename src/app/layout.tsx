d54d5e07bff23ef8e2a4d9427ec1a4d26c04d402
Maxim Shmelov <mokinous@gmail.com>
2026-02-01 14:57:26 +0000

    updated CollectionClient.tsx design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,25 +8,21 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body>
-        <div className="flex min-h-screen">
-          <div>
-            {/* DESKTOP SIDEBAR */}
-            <aside className="hidden md:block w-64">
-              <DesktopSidebar />
-            </aside>
-          </div>
+        <div className="flex min-h-screen overflow-x-hidden">
+          <aside className="hidden md:block w-64 shrink-0">
+            <DesktopSidebar />
+          </aside>
 
-          <div className="flex-1">
-            <div className="flex flex-row w-full">
+          <div className="flex-1 min-w-0">
+            <div className="flex flex-row w-full min-w-0">
               <MobileSidebar />
               <SessionProviderWrapper>
                 <Header />
               </SessionProviderWrapper>
             </div>
 
-            {/* MAIN */}
-            <main className="flex-1 px-4">
-              <div className="max-w-3xl mx-auto p-4">{children}</div>
+            <main className="flex-1 px-4 w-full min-w-0">
+              <div className="w-full max-w-3xl mx-auto p-4 min-w-0">{children}</div>
             </main>
           </div>
         </div>

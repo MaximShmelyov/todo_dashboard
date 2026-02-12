130ea766ed50dc3b9d30574033cafa01092b9de3
Maxim Shmelov <mokinous@gmail.com>
2026-02-12 15:05:44 +0000

    Use ModalDialog in ConfirmPopup.tsx

--- src/components/layout/ConfirmPopup.tsx
+++ src/components/layout/ConfirmPopup.tsx
@@ -1,45 +1,30 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
+import ModalDialog from "@/src/components/common/ModalDialog";
 import Button from "@/src/components/ui/Button";
 
 export default function ConfirmPopup({
   title,
   onCancelAction,
   onConfirmAction,
+  open = true,
 }: {
   title: string;
   onCancelAction: () => void;
   onConfirmAction: () => void;
+  open?: boolean;
 }) {
   return (
-    <AnimatePresence>
-      <motion.div
-        className="fixed inset-0 bg-black/40 flex justify-center items-center"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        onClick={() => onCancelAction()}
-      >
-        <motion.div
-          className="flex flex-col gap-8
-                     bg-white dark:bg-stone-800
-                     p-6 rounded-sm shadow-xl w-60"
-          initial={{ scale: 0.9, opacity: 0, y: 32 }}
-          animate={{ scale: 1, opacity: 1, y: 0 }}
-          exit={{ scale: 0.9, opacity: 0, y: -32 }}
-          transition={{ type: "spring", stiffness: 400, damping: 30 }}
-          onClick={(e) => e.stopPropagation()}
-        >
-          <div className="font-bold text-center">{title}</div>
-          <div className="flex flex-row justify-around">
-            <Button variant={"delete"} onClick={() => onConfirmAction()}>
-              Yes
-            </Button>
-            <Button onClick={() => onCancelAction()}>No</Button>
-          </div>
-        </motion.div>
-      </motion.div>
-    </AnimatePresence>
+    <ModalDialog open={open} onCloseAction={onCancelAction}>
+      <div className="flex flex-col gap-8 w-60">
+        <div className="font-bold text-center">{title}</div>
+        <div className="flex flex-row justify-around">
+          <Button variant={"delete"} onClick={onConfirmAction}>
+            Yes
+          </Button>
+          <Button onClick={onCancelAction}>No</Button>
+        </div>
+      </div>
+    </ModalDialog>
   );
 }

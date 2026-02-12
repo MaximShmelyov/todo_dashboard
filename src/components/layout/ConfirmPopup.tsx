162eb972d7e7c6e7b6d8d98c162b9de02d737439
Maxim Shmelov <mokinous@gmail.com>
2026-02-12 14:24:37 +0000

    Animate ConfirmPopup with framer-motion

--- src/components/layout/ConfirmPopup.tsx
+++ src/components/layout/ConfirmPopup.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AnimatePresence, motion } from "framer-motion";
 import Button from "@/src/components/ui/Button";
 
 export default function ConfirmPopup({
@@ -12,24 +13,33 @@ export default function ConfirmPopup({
   onConfirmAction: () => void;
 }) {
   return (
-    <div
-      className="fixed inset-0 bg-black/40 flex justify-center items-center"
-      onClick={() => onCancelAction()}
-    >
-      <div
-        className="flex flex-col gap-8
-                   bg-white dark:bg-stone-800
-                   p-6 rounded-sm shadow-xl w-60"
-        onClick={(e) => e.stopPropagation()}
+    <AnimatePresence>
+      <motion.div
+        className="fixed inset-0 bg-black/40 flex justify-center items-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        onClick={() => onCancelAction()}
       >
-        <div className="font-bold text-center">{title}</div>
-        <div className="flex flex-row justify-around">
-          <Button variant={"delete"} onClick={() => onConfirmAction()}>
-            Yes
-          </Button>
-          <Button onClick={() => onCancelAction()}>No</Button>
-        </div>
-      </div>
-    </div>
+        <motion.div
+          className="flex flex-col gap-8
+                     bg-white dark:bg-stone-800
+                     p-6 rounded-sm shadow-xl w-60"
+          initial={{ scale: 0.9, opacity: 0, y: 32 }}
+          animate={{ scale: 1, opacity: 1, y: 0 }}
+          exit={{ scale: 0.9, opacity: 0, y: -32 }}
+          transition={{ type: "spring", stiffness: 400, damping: 30 }}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="font-bold text-center">{title}</div>
+          <div className="flex flex-row justify-around">
+            <Button variant={"delete"} onClick={() => onConfirmAction()}>
+              Yes
+            </Button>
+            <Button onClick={() => onCancelAction()}>No</Button>
+          </div>
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
